08f4cf9fc78fcf1b6ce4699d1452421a32c010e8
RealDeviil <real_deviil@yahoo.com>
2023-04-19 17:27:37 -0400

    Fixed "Download" misspelling

--- iframe/convert.php
+++ iframe/convert.php
@@ -136,7 +136,7 @@ if (Request::isPost())
                             elem.innerHTML = `
                                 <a href="${data.download}" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" class="inline-block h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
                                     <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg> Downlaod MP3</a>`;
+                                </svg> Download MP3</a>`;
                         } 
                         if (status == 'failed') {
                             elem.innerHTML = `Oops, conversion failed.`;
